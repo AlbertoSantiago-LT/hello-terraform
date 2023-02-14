33102930113680f5bb010d19026853d96f35118e
Alberto Santiago <albertosantiago97gt@gmail.com>
2023-02-14 17:18:49 +0100

    Correcion del main y agregar al gitignore terraform.ftstate

--- main.tf
+++ main.tf
@@ -1,27 +1,27 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 4.16"
     }
   }
-  
 
-  required_version= ">= 1.2.0"
+
+  required_version = ">= 1.2.0"
 }
 
 
 provider "aws" {
-  region= "eu-west-1"
+  region = "eu-west-1"
 }
 
 
 resource "aws_instance" "app_server" {
-  ami = ami-0b752bf1df193a6c4
-  instances_type = "t2.micro"
+  ami            = "ami-0b752bf1df193a6c4"
+  instance_type = "t2.micro"
 
-  tag = {
+  tags = {
     Name = "Terraform"
-    APP = "vue2048"
+    APP  = "vue2048"
   }
 }
